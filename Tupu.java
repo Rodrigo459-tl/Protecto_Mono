65a4aeee2c8e641f691d7af7b07521cd07171170
Roy <rodrigo.osorio.rl@gmail.com>
2024-05-28 13:12:03 -0600

    "DDDD"

--- Tupu.java
+++ Tupu.java
@@ -2,6 +2,8 @@ public class Tupu{
     int a;
     int b;
     int resultado;
+    String nombre;
+    String ddd;
     public static void main(String[] args) {
         System.out.println("Andy gei");
     }
