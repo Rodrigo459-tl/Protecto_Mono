a6b2a419fd7b79520ce266d205a26d38d14e895f
Roy <rodrigo.osorio.rl@gmail.com>
2024-05-28 13:34:09 -0600

    "Comenzar el proyecto mono"

--- Tupu.java
+++ Tupu.java
@@ -1,11 +1,5 @@
 public class Tupu{
-    int a;
-    int b;
-    int resultado;
-    String nombre;
-    String ddd;
     public static void main(String[] args) {
-        int a,b = 8;
-        System.out.println("Andy gei");
+        
     }
 }
