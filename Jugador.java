d975f1b05658ee1d4cfb433d4764e6d07cb193ef
Roy <rodrigo.osorio.rl@gmail.com>
2024-05-28 13:47:36 -0600

    "Juego"

---
+++ Jugador.java
@@ -0,0 +1,18 @@
+public class Jugador {
+
+    //ATRIBUTOS
+    private String gamerTag;
+
+    //GETTERS AND SETTERS
+    public String getGamerTag() {
+        return gamerTag;
+    }
+    public void setGamerTag(String gamerTag) {
+        this.gamerTag = gamerTag;
+    }
+    //METODOS
+    public String generarGT(){
+        String GT= "Aniquilador";
+        return GT;
+    }
+}
--- Tupu.java
+++ Tupu.java
@@ -1,5 +1,6 @@
 public class Tupu{
     public static void main(String[] args) {
         
+
     }
 }
